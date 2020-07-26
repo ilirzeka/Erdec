4813b654ce51e10fb8120e1c858e323df9a3f10b
ilirzeka <ilirzeka01@gmail.com>
2020-07-26 16:35:18 +0200

    Logo ne navbar

--- js/main.js
+++ js/main.js
@@ -220,23 +220,63 @@
   $('[data-toggle="popover"]').popover()
 	$('[data-toggle="tooltip"]').tooltip()
 
-// When the user scrolls the page, execute myFunction
+
+
+	// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("ftco-navbar");
+var header = document.getElementById("ftco-navbar")
+
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
+
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+if (window.pageYOffset > sticky) {
+	header.classList.add("sticky");
+	
+	
+ 	} else {
+	header.classList.remove("sticky");
+	
+
+ 	}
+	
 }
+ 
+if($(window).width() < 992) { 
+
+	// When the user scrolls the page, execute myFunction
+	window.onscroll = function() {myFunction2()};
+
+	// Get the header
+	var header = document.getElementById("ftco-navbar")
+	
+	
+	// Get the offset position of the navbar
+	var sticky = header.offsetTop;
+	
+	
+	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction2() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky img");
+		
+		
+		 } else {
+		header.classList.remove("sticky img");
+		
+	
+		 }
+		
+	}
+	 
+}
+
+
 
 })(jQuery);
 
